e9da146aa4a5de87727c2a9e1f56c23cd45adfc9
Mazennn004 <mazen.hamad07@gmail.com>
2025-09-25 07:02:58 +0300

    padding

--- src/app/_components/Footer/Footer.tsx
+++ src/app/_components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div className='w-full flex justify-center bg-[#1f0a33] text-white p-10'>
+    <div className='w-full flex justify-center bg-[#1f0a33] text-white p-5'>
       <span>© 2025 Aura MH GROUP Commerce, All rights reserved</span>
     </div>
   )
